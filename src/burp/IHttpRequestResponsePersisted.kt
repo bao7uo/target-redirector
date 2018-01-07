87512c08c169fca1c6999d6714eeb9bf8e6c6ba5
Paul Taylor <21125224+bao7uo@users.noreply.github.com>
2018-01-07 23:34:07 +0000

    Initial source upload

---
+++ src/burp/IHttpRequestResponsePersisted.kt
@@ -0,0 +1,24 @@
+package burp
+
+/*
+ * @(#)IHttpRequestResponsePersisted.java
+ *
+ * Copyright PortSwigger Ltd. All rights reserved.
+ *
+ * This code may be used to extend the functionality of Burp Suite Community Edition
+ * and Burp Suite Professional, provided that this usage does not violate the
+ * license terms for those products.
+ */
+/**
+ * This interface is used for an
+ * `IHttpRequestResponse` object whose request and response messages
+ * have been saved to temporary files using
+ * `IBurpExtenderCallbacks.saveBuffersToTempFiles()`.
+ */
+interface IHttpRequestResponsePersisted : IHttpRequestResponse {
+    /**
+     * This method is deprecated and no longer performs any action.
+     */
+    @Deprecated("This method is deprecated and no longer performs any action.")
+    fun deleteTempFiles()
+}
