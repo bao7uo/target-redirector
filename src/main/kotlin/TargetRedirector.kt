635e53bb75e7b24edcf03797fb03b79e5425260c
Paul Taylor <21125224+bao7uo@users.noreply.github.com>
2018-01-08 21:27:47 +0000

    Update TargetRedirector.kt

--- src/main/kotlin/TargetRedirector.kt
+++ src/main/kotlin/TargetRedirector.kt
@@ -3,7 +3,7 @@
 #
 #   Target Redirector Burp extension
 #
-#   Copyright (C) 2017 Paul Taylor
+#   Copyright (C) 2016-2018 Paul Taylor
 #
 
 # Licensed under the Apache License, Version 2.0 (the "License");
