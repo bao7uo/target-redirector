20414614c84f3965e845b065c899e2baba017d72
bao7uo <21125224+bao7uo@users.noreply.github.com>
2018-01-12 03:20:19 +0000

    New features: ports/proto redirection

--- src/main/kotlin/target-redirector.kt
+++ src/main/kotlin/target-redirector.kt
@@ -34,15 +34,30 @@ import javax.swing.JButton
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.JTextField
+import javax.swing.JCheckBox
 
 class Redirector {
     companion object {
-        var original = ""
-        var replacement = ""
+        var original_host = ""
+        var replacement_host = ""
+        var original_port = ""
+        var replacement_port = ""
+        var original_protocol = ""
+        var replacement_protocol = ""
+
         fun update(data_source: UI) {
-            original = data_source.text_left.text
-            replacement = data_source.text_right.text
-            PrintWriter(data_source.callbacks.stdout, true).println("Redirection Activated. Will replace: ${original} With: ${replacement}")
+            original_host = data_source.text_host_original.text
+            replacement_host = data_source.text_host_replacement.text
+            original_port = data_source.text_port_original.text
+            replacement_port = data_source.text_port_replacement.text
+            original_protocol = if (data_source.cbox_https_original.isSelected()) "https" else "http"
+            replacement_protocol = if (data_source.cbox_https_replacement.isSelected()) "https" else "http"
+            PrintWriter(
+                    data_source.callbacks.stdout,
+                    true
+            ).println(
+                    "Redirection Activated. Will replace: ${original_host}:${original_port}/${original_protocol} With: ${replacement_host}:${replacement_port}/${replacement_protocol}"
+            )
         }
     }
 }
@@ -57,12 +72,26 @@ class UI(val callbacks: IBurpExtenderCallbacks) : ITab {
 
     val button = JButton("Activate Redirection")
 
-    val textpanel_left = JPanel()
-    val label_left = JLabel("Look for:")
-    val text_left = JTextField(20)
-    val textpanel_right = JPanel()
-    val label_right = JLabel("Replace with:")
-    val text_right = JTextField(20)
+    val textpanel_original = JPanel()
+    val label_host_original = JLabel("for host/IP")
+    val text_host_original = JTextField(20)
+    val label_port_original = JLabel("on port")
+    val text_port_original = JTextField(5)
+
+    val cbox_https_original = JCheckBox("with HTTPS")
+
+    val textpanel_replacement = JPanel()
+    val label_host_replacement = JLabel("to: host/IP")
+    val text_host_replacement = JTextField(20)
+    val label_port_replacement = JLabel("on port")
+    val text_port_replacement = JTextField(5)
+
+    val cbox_https_replacement = JCheckBox("with HTTPS")
+
+    val textpanel_options = JPanel()
+
+    val cbox_hostheader = JCheckBox("Also replace HTTP host header")
+    val cbox_dns_required = JCheckBox("Fix DNS for original host")
 
     fun button_pressed(e: ActionEvent) {
         Redirector.update(this)
@@ -84,24 +113,39 @@ class UI(val callbacks: IBurpExtenderCallbacks) : ITab {
         innerpanel.add(subpanel_upper)
         innerpanel.add(subpanel_lower)
 
-        subpanel_upper.border = BorderFactory.createTitledBorder("Replaces matched target hostname/IP in all HTTP requests")
+        subpanel_upper.border = BorderFactory.createTitledBorder("Redirect ALL connections")
+        subpanel_upper.layout = BoxLayout(subpanel_upper, BoxLayout.Y_AXIS)
 
-        subpanel_upper.add(textpanel_left)
-        subpanel_upper.add(textpanel_right)
+        subpanel_upper.add(textpanel_original)
+        subpanel_upper.add(textpanel_replacement)
+        subpanel_upper.add(textpanel_options)
 
-        textpanel_left.add(label_left)
-        textpanel_left.add(text_left)
+        textpanel_original.add(label_host_original)
+        textpanel_original.add(text_host_original)
+        textpanel_original.add(label_port_original)
+        textpanel_original.add(text_port_original)
+        textpanel_original.add(cbox_https_original)
 
-        textpanel_right.add(label_right)
-        textpanel_right.add(text_right)
+        textpanel_replacement.add(label_host_replacement)
+        textpanel_replacement.add(text_host_replacement)
+        textpanel_replacement.add(label_port_replacement)
+        textpanel_replacement.add(text_port_replacement)
+        textpanel_replacement.add(cbox_https_replacement)
+
+        textpanel_options.add(cbox_hostheader)
+        textpanel_options.add(cbox_dns_required)
+
+        cbox_hostheader.setEnabled(false)
+        cbox_dns_required.setEnabled(false)
 
         subpanel_lower.layout = BoxLayout(subpanel_lower, BoxLayout.X_AXIS)
 
         subpanel_lower.add(Box.createHorizontalGlue())
         subpanel_lower.add(button)
+        subpanel_lower.add(Box.createVerticalGlue())
 
-        textpanel_left.maximumSize = textpanel_left.preferredSize
-        textpanel_right.maximumSize = textpanel_right.preferredSize
+        textpanel_original.maximumSize = textpanel_original.preferredSize
+        textpanel_replacement.maximumSize = textpanel_replacement.preferredSize
         subpanel_upper.maximumSize = subpanel_upper.preferredSize
         subpanel_lower.maximumSize = subpanel_lower.preferredSize
         innerpanel.maximumSize = innerpanel.preferredSize
@@ -128,20 +172,27 @@ class HttpListener(val callbacks: IBurpExtenderCallbacks) : IHttpListener {
         var stdout = PrintWriter(callbacks.stdout, true)
 
         if (messageIsRequest) {
-            stdout.println("Searching for: ${Redirector.original}")
-            stdout.println("Incoming request to: ${messageInfo.httpService.host}")
-            if (messageInfo.httpService.host == Redirector.original) {
+            stdout.println("----->")
+            stdout.println("> Searching for: ${Redirector.original_host}:${Redirector.original_port}/${Redirector.original_protocol}")
+            stdout.println("> Incoming request to: ${messageInfo.httpService.host}:${messageInfo.httpService.port.toString()}/${messageInfo.httpService.protocol}")
+            if (messageInfo.httpService.host == Redirector.original_host
+                    && messageInfo.httpService.port == Redirector.original_port.toIntOrNull()
+                    && (messageInfo.httpService.protocol == Redirector.original_protocol)
+                    ) {
                 messageInfo.httpService = callbacks.helpers.buildHttpService(
-                        Redirector.replacement,
-                        messageInfo.httpService.port,
-                        messageInfo.httpService.protocol
+                        Redirector.replacement_host,
+                        Redirector.replacement_port.toInt(),
+                        Redirector.replacement_protocol
                 )
-                stdout.println("Target changed from ${Redirector.original} to ${Redirector.replacement}")
+                stdout.println(
+                        "> Target changed from ${Redirector.original_host}:${Redirector.original_port}/${Redirector.original_protocol} to ${Redirector.replacement_host}:${Redirector.replacement_port}/${Redirector.replacement_protocol}"
+                )
             } else {
-                stdout.println("Target not changed to ${Redirector.replacement}")
+                stdout.println("> Target not changed to ${Redirector.replacement_host}:${Redirector.replacement_port}/${Redirector.replacement_protocol}")
             }
         } else {
-            stdout.println("Incoming response from: ${messageInfo.httpService.host}")
+            stdout.println("<-----")
+            stdout.println("< Incoming response from: ${messageInfo.httpService.host}:${messageInfo.httpService.port.toString()}/${messageInfo.httpService.protocol}")
         }
     }
 }
