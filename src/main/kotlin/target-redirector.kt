e0ad3c4373ead17dc7d7d08dd13ee0e37cec1c2f
bao7uo <21125224+bao7uo@users.noreply.github.com>
2018-01-14 06:43:10 +0000

    Improved code to prep for new features

--- src/main/kotlin/target-redirector.kt
+++ src/main/kotlin/target-redirector.kt
@@ -25,8 +25,6 @@ package burp
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 
-import java.io.PrintWriter
-
 import java.net.InetAddress
 
 import javax.swing.BorderFactory
@@ -40,114 +38,150 @@ import javax.swing.JCheckBox
 import javax.swing.JOptionPane
 
 
-class Redirector {
+class HttpListener(val cb: IBurpExtenderCallbacks, val redirector: Redirector) : IHttpListener {
 
-    companion object {
-        var original_host = ""
-        var replacement_host = ""
-        var original_port = ""
-        var replacement_port = ""
-        var original_protocol = ""
-        var replacement_protocol = ""
+    override fun processHttpMessage(
+            toolFlag: Int,
+            messageIsRequest: Boolean,
+            messageInfo: IHttpRequestResponse
+    ) {
+        redirector.process_redirect(messageIsRequest, messageInfo)
+    }
+}
 
-        var json_backup = ""
 
-        fun original() = "${original_protocol}://${original_host}:${original_port}"
-        fun replacement() = "${replacement_protocol}://${replacement_host}:${replacement_port}"
+class Redirector(val cb: IBurpExtenderCallbacks) {
 
-        fun stop(data_source: UI) {
-            data_source.button.text = "Activate redirection"
-            original_port = "ZZZZ"
-            replacement_port = "ZZZZ"
-            data_source.cbox_dns_required.setSelected(false)
-            if (json_backup != "") {
-                data_source.callbacks.loadConfigFromJson(json_backup)
-                json_backup = ""
+    var listener = HttpListener(cb, this)
+
+    var active = false
+        
+    lateinit var original: Map<String, String>
+    lateinit var replacement: Map<String, String>
+
+    var json_backup = ""
+
+    fun original_url() = "${original["protocol"]}://${original["host"]}:${original["port"]}"
+    fun replacement_url() = "${replacement["protocol"]}://${replacement["host"]}:${replacement["port"]}"
+
+    fun process_redirect(messageIsRequest: Boolean, messageInfo: IHttpRequestResponse) {
+        val current = "${messageInfo.httpService.protocol}://${messageInfo.httpService.host}:${messageInfo.httpService.port.toString()}"
+
+        if (!active) {return}
+
+        if (messageIsRequest) {
+            cb.printOutput("----->")
+            cb.printOutput("> Searching for: ${original_url()}")
+            cb.printOutput("> Incoming request to: ${current}")
+            if (messageInfo.httpService.host == original["host"]
+                    && messageInfo.httpService.port == original["port"]?.toIntOrNull()
+                    && (messageInfo.httpService.protocol == original["protocol"])
+                    ) {
+                messageInfo.httpService = cb.helpers.buildHttpService(
+                        replacement["host"],
+                        replacement["port"]!!.toInt(),
+                        replacement["protocol"]
+                )
+                cb.printOutput(
+                        "> Target changed from ${original_url()} to ${replacement_url()}"
+                )
+            } else {
+                cb.printOutput("> Target not changed to ${replacement_url()}")
+            }
+        } else {
+            cb.printOutput("<-----")
+            cb.printOutput("< Incoming response from: ${current}")
+        }        
+    }
+
+    fun toggle(data_source: UI, mainpanel: JPanel, original_data: Map<String, String>, replacement_data: Map<String, String>) {
+        if (active) {
+            stop(data_source)
+        } else  update(data_source, mainpanel, original_data, replacement_data)
+    }
+
+    fun stop(data_source: UI) {
+
+        cb.removeHttpListener(listener)
+        active = false
+        data_source.toggle_active(false)
+
+        if (json_backup != "") {
+            cb.loadConfigFromJson(json_backup)
+            json_backup = ""
+        }
+    }
+
+    fun update(data_source: UI, mainpanel: JPanel, original_data: Map<String, String>, replacement_data: Map<String, String>) {
+
+        original = original_data
+        replacement = replacement_data
+
+        fun getbyname(name: String): Boolean {
+            try {
+                InetAddress.getByName(name)
+                return true
+            }
+            catch (UnknownHostException: Exception) {
+                cb.printOutput("Hostname/IP \"${name}\" appears to be invalid.")
+                return false
             }
         }
 
-        fun update(data_source: UI) {
+        fun notification(text: String) {
+            JOptionPane.showMessageDialog(mainpanel, text, "Burp / Target Redirector", JOptionPane.WARNING_MESSAGE)
+            cb.printOutput(text) 
+        }
 
-            var stdout = PrintWriter(
-                                data_source.callbacks.stdout,
-                                true
-                            )
+        if (
+            !original["host"].isNullOrBlank() &&
+            original["port"]?.toIntOrNull() != null &&
+            !replacement["host"].isNullOrBlank() &&
+            replacement["port"]?.toIntOrNull() != null &&
+            getbyname(replacement["host"]!!)
+            ) {
+                if (!getbyname(original["host"]!!)) {
+                    notification("Original hostname \"${original["host"]}\" appears to be invalid.\n\n" +
+                        "Target Redirector will add an entry to\nProject options / Hostname Resolution\n" +
+                        "to allow Burp to send requests with an\n" +
+                        "invalid hostname via this extension.")
 
-            val _original_host = data_source.text_host_original.text
-            val _replacement_host = data_source.text_host_replacement.text
-            val _original_port = data_source.text_port_original.text
-            val _replacement_port = data_source.text_port_replacement.text
-            val _original_protocol = if (data_source.cbox_https_original.isSelected()) "https" else "http"
-            val _replacement_protocol = if (data_source.cbox_https_replacement.isSelected()) "https" else "http"
-
-            fun getbyname(name: String): Boolean {
-                try {
-                    InetAddress.getByName(name)
-                    return true
-                }
-                catch (UnknownHostException: Exception) {
-                    stdout.println("Hostname/IP \"${name}\" appears to be invalid.")
-                    return false
-                }
-            }
-
-            fun notification(text: String) {
-                JOptionPane.showMessageDialog(data_source.mainpanel, text, "Burp / Target Redirector", JOptionPane.WARNING_MESSAGE)
-                stdout.println(text) 
-            }
-
-            if (
-                _original_host != "" &&
-                _original_port.toIntOrNull() != null &&
-                _replacement_host != "" &&
-                _replacement_port.toIntOrNull() != null &&
-                getbyname(_replacement_host)
-                ) {
-                    if (!getbyname(_original_host)) {
-                        notification("Original hostname \"${_original_host}\" appears to be invalid.\n\n" +
-                            "Target Redirector will add an entry to\nProject options / Hostname Resolution\n" +
-                            "to allow Burp to send requests with an\n" +
-                            "invalid hostname via this extension.")
-
-                        var json_config = data_source.callbacks.saveConfigAsJson(
-                            "project_options.connections.hostname_resolution"
-                        )
-
-                        json_backup = json_config
-
-                        var json_snippet = "{\"enabled\":true,\"hostname\":\"" +
-                                            _original_host +
-                                            "\",\"ip_address\":\"127.0.0.1\"}" +
-                                            if (json_config.indexOf("ip_address") >= 0) "," else ""
-                        json_config = json_config.substring(0, 89) + json_snippet + json_config.substring(89, json_config.length)
-                        data_source.callbacks.loadConfigFromJson(json_config)
-
-                        data_source.cbox_dns_required.setSelected(true)
-                    } else {
-                        data_source.cbox_dns_required.setSelected(false)
-                    }
-
-                    original_host = _original_host
-                    replacement_host = _replacement_host
-                    original_port = _original_port
-                    replacement_port = _replacement_port
-                    original_protocol = _original_protocol
-                    replacement_protocol = _replacement_protocol
-
-                    notification(
-                        "Redirection Activated.\nTarget Redirector is now\nredirecting requests for:\n${original()}\nto:\n${replacement()}"
+                    var json_config = cb.saveConfigAsJson(
+                        "project_options.connections.hostname_resolution"
                     )
 
-                    data_source.button.text = "Remove redirection"
-            } else {
-                    notification("Invalid hostname and/or port settings.")
-            }
+                    json_backup = json_config
+
+                    var json_snippet = "{\"enabled\":true,\"hostname\":\"" +
+                                        original["host"] +
+                                        "\",\"ip_address\":\"127.0.0.1\"}" +
+                                        if (json_config.indexOf("ip_address") >= 0) "," else ""
+                    json_config = json_config.substring(0, 85) + json_snippet + json_config.substring(85, json_config.length)
+                    cb.loadConfigFromJson(json_config)
+
+                    data_source.toggle_dns_correction(true)
+                } else {
+                    data_source.toggle_dns_correction(false)
+                }
+
+                notification(
+                    "Redirection Activated.\nTarget Redirector is now\nredirecting requests for:\n${original_url()}\nto:\n${replacement_url()}"
+                )
+
+                cb.registerHttpListener(listener)
+                active = true
+                data_source.toggle_active(true)
+        } else {
+                notification("Invalid hostname and/or port settings.")
         }
     }
 }
 
 
-class UI(val callbacks: IBurpExtenderCallbacks) : ITab {
+class UI(val cb: IBurpExtenderCallbacks, val redirector: Redirector) : ITab {
+
+    override public fun getTabCaption() = "Target Redirector"
+    override public fun getUiComponent() = mainpanel
 
     val mainpanel = JPanel()
     val innerpanel = JPanel()
@@ -155,38 +189,64 @@ class UI(val callbacks: IBurpExtenderCallbacks) : ITab {
     val subpanel_upper = JPanel()
     val subpanel_lower = JPanel()
 
+    class redirect_panel(val host: String) : JPanel() {
+
+        val label_host = JLabel(host)       
+        val text_host = JTextField(20)
+
+        val label_port = JLabel("on port")
+        val text_port = JTextField(5)
+
+        val cbox_https = JCheckBox("with HTTPS")  
+
+        init {
+            add(label_host)
+            add(text_host)
+            add(label_port)
+            add(text_port)
+            add(cbox_https)
+            maximumSize = preferredSize
+        }
+
+        fun get_data(): Map<String, String> {
+            val data = mutableMapOf<String, String>()
+            data["host"] = text_host.text
+            data["port"] = text_port.text
+            data["protocol"] = if (cbox_https.isSelected()) "https" else "http"
+            return data
+        }
+
+        fun toggle_lock(locked: Boolean) {
+            text_host.setEditable(locked)
+            text_port.setEditable(locked)
+            cbox_https.setEnabled(locked)
+        }
+    }
+
+    val redirect_panel_original = redirect_panel("for host/IP")
+    val redirect_panel_replacement = redirect_panel("to: host/IP")
+
+    fun toggle_active(active: Boolean) {
+        button.text = if (active) "Remove redirection" else "Activate redirection"
+        redirect_panel_original.toggle_lock(!active)
+        redirect_panel_replacement.toggle_lock(!active)
+        if (!active) { cbox_dns_correction.setSelected(false) }
+    }
+
+    fun toggle_dns_correction(enabled: Boolean) {
+        cbox_dns_correction.setSelected(enabled)
+    }
+
+    val redirect_panel_options = JPanel()
+    val cbox_hostheader = JCheckBox("Also replace HTTP host header")
+    val cbox_dns_correction = JCheckBox("Invalid original hostname DNS correction")
+
     val button = JButton("Activate Redirection")
 
-    val textpanel_original = JPanel()
-    val label_host_original = JLabel("for host/IP")
-    val text_host_original = JTextField(20)
-    val label_port_original = JLabel("on port")
-    val text_port_original = JTextField(5)
-
-    val cbox_https_original = JCheckBox("with HTTPS")
-
-    val textpanel_replacement = JPanel()
-    val label_host_replacement = JLabel("to: host/IP")
-    val text_host_replacement = JTextField(20)
-    val label_port_replacement = JLabel("on port")
-    val text_port_replacement = JTextField(5)
-
-    val cbox_https_replacement = JCheckBox("with HTTPS")
-
-    val textpanel_options = JPanel()
-
-    val cbox_hostheader = JCheckBox("Also replace HTTP host header")
-    val cbox_dns_required = JCheckBox("Invalid original hostname DNS correction")
-
-    fun button_pressed(e: ActionEvent) {
-        if (button.text != "Remove redirection") Redirector.update(this)
-        else Redirector.stop(this)
+    fun button_pressed() {
+        redirector.toggle(this, mainpanel, redirect_panel_original.get_data(), redirect_panel_replacement.get_data())
     }
     
-    override public fun getTabCaption() = "Target Redirector"
-
-    override public fun getUiComponent() = mainpanel
-
     init {
         mainpanel.layout = BoxLayout(mainpanel, BoxLayout.Y_AXIS)
         mainpanel.border = BorderFactory.createEmptyBorder(20, 20, 20, 20)
@@ -202,27 +262,15 @@ class UI(val callbacks: IBurpExtenderCallbacks) : ITab {
         subpanel_upper.border = BorderFactory.createTitledBorder("Redirect all Burp connections")
         subpanel_upper.layout = BoxLayout(subpanel_upper, BoxLayout.Y_AXIS)
 
-        subpanel_upper.add(textpanel_original)
-        subpanel_upper.add(textpanel_replacement)
-        subpanel_upper.add(textpanel_options)
+        subpanel_upper.add(redirect_panel_original)
+        subpanel_upper.add(redirect_panel_replacement)
+        subpanel_upper.add(redirect_panel_options)
 
-        textpanel_original.add(label_host_original)
-        textpanel_original.add(text_host_original)
-        textpanel_original.add(label_port_original)
-        textpanel_original.add(text_port_original)
-        textpanel_original.add(cbox_https_original)
-
-        textpanel_replacement.add(label_host_replacement)
-        textpanel_replacement.add(text_host_replacement)
-        textpanel_replacement.add(label_port_replacement)
-        textpanel_replacement.add(text_port_replacement)
-        textpanel_replacement.add(cbox_https_replacement)
-
-        textpanel_options.add(cbox_hostheader)
-        textpanel_options.add(cbox_dns_required)
+        redirect_panel_options.add(cbox_hostheader)
+        redirect_panel_options.add(cbox_dns_correction)
 
         cbox_hostheader.setEnabled(false)
-        cbox_dns_required.setEnabled(false)
+        cbox_dns_correction.setEnabled(false)
 
         subpanel_lower.layout = BoxLayout(subpanel_lower, BoxLayout.X_AXIS)
 
@@ -230,8 +278,6 @@ class UI(val callbacks: IBurpExtenderCallbacks) : ITab {
         subpanel_lower.add(button)
         subpanel_lower.add(Box.createVerticalGlue())
 
-        textpanel_original.maximumSize = textpanel_original.preferredSize
-        textpanel_replacement.maximumSize = textpanel_replacement.preferredSize
         subpanel_upper.maximumSize = subpanel_upper.preferredSize
         subpanel_lower.maximumSize = subpanel_lower.preferredSize
         innerpanel.maximumSize = innerpanel.preferredSize
@@ -240,68 +286,26 @@ class UI(val callbacks: IBurpExtenderCallbacks) : ITab {
         button.addActionListener(
                 object : ActionListener {
                     override fun actionPerformed(e: ActionEvent) {
-                        button_pressed(e)
+                        if (!true && e.actionCommand == "") {}  // hack to remove compiler warning
+                        button_pressed()                        // about e argument being unused
                     }
                 }
         )
+
+        cb.customizeUiComponent(mainpanel)
     }
 
 }
-
-class HttpListener(val callbacks: IBurpExtenderCallbacks) : IHttpListener {
-
-    override fun processHttpMessage(
-            toolFlag: Int,
-            messageIsRequest: Boolean,
-            messageInfo: IHttpRequestResponse
-    ) {
-        var stdout = PrintWriter(callbacks.stdout, true)
-        val current = "${messageInfo.httpService.protocol}://${messageInfo.httpService.host}:${messageInfo.httpService.port.toString()}"
-
-        if (messageIsRequest) {
-            stdout.println("----->")
-            stdout.println("> Searching for: ${Redirector.original()}")
-            stdout.println("> Incoming request to: ${current}")
-            if (messageInfo.httpService.host == Redirector.original_host
-                    && messageInfo.httpService.port == Redirector.original_port.toIntOrNull()
-                    && (messageInfo.httpService.protocol == Redirector.original_protocol)
-                    ) {
-                messageInfo.httpService = callbacks.helpers.buildHttpService(
-                        Redirector.replacement_host,
-                        Redirector.replacement_port.toInt(),
-                        Redirector.replacement_protocol
-                )
-                stdout.println(
-                        "> Target changed from ${Redirector.original()} to ${Redirector.replacement()}"
-                )
-            } else {
-                stdout.println("> Target not changed to ${Redirector.replacement()}")
-            }
-        } else {
-            stdout.println("<-----")
-            stdout.println("< Incoming response from: ${current}")
-        }
-    }
-}
-
 
 class BurpExtender : IBurpExtender {
 
-    override fun registerExtenderCallbacks(callbacks: IBurpExtenderCallbacks) {
+    override fun registerExtenderCallbacks(cb: IBurpExtenderCallbacks) {       
+        
+        val tab = UI(cb, Redirector(cb))
 
-        var stdout = PrintWriter(callbacks.stdout, true)
-
-        val httplistener = HttpListener(callbacks)
-        val tab = UI(callbacks)
-
-        callbacks.setExtensionName("Target Redirector")
-
-        callbacks.registerHttpListener(httplistener)
-
-        callbacks.customizeUiComponent(tab.mainpanel)
-        callbacks.addSuiteTab(tab)
-
-        stdout.println("Target Redirector extension loaded")
+        cb.setExtensionName("Target Redirector")
+        cb.addSuiteTab(tab)
+        cb.printOutput("Target Redirector extension loaded")
     }
 
 }
